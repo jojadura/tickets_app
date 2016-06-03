a273d8572338e00c14d4661716dd2dbb74c17393
John Jairo <jojadura@gmail.com>
2016-06-03 11:57:45 -0500

    Ajuste Helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,6 @@
 module CategoriesHelper
 	def form_title(model)
-		model.new_record? ? "Crear Categoría" : "Editar Categoría"		
+		#model.new_record? ? "Crear Categoría" : "Editar Categoría"
+		model.new_record? ? "Crear #{model.class}" : "Editar #{model.class}"
 	end		
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'test/index'
+
   resources :categories
   resources :sub_categories
 
